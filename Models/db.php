3ada684f864e1738d3a259505a4bc61663781e1c
Alej0sebs <arivera1806@uta.edu.ec>
2025-07-01 01:01:08 -0500

    xd

--- Models/db.php
+++ Models/db.php
@@ -8,6 +8,6 @@ $dbname = getenv('DB_NAME') ?: 'railway';
 $conn = new mysqli($host, $user, $pass, $dbname, $port);
 
 if ($conn->connect_error) {
-    die(json_encode(['errorMsg' => 'Error de conexión: ' . $conn->connect_error]));
+    die(json_encode(['errorMsg' => 'Error de conexión : ' . $conn->connect_error]));
 }
 ?>
